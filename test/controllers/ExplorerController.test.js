35c87cb14f58a1e60ec9b2a9e46db97f88362d7c
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-04-27 19:54:21 -0500

    add: ExplorerController second method test

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -4,5 +4,6 @@ describe("Unit tests for Explorer Controller", () => {
     test("Use the Explorer Controller", () => {
         const explorers = [{name:"Explorer1"}]
         expect(ExplorerController.getExplorersByMission("node").length).toBe(10);
+        expect(ExplorerController.getExplorersUsernamesByMission("node").length).toBe(10);
     });
 });
