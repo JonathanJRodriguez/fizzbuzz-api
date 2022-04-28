e65cec2556421a5f9a0ffd4b028194f61d72f06b
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-04-28 18:13:01 -0500

    add: Test for new feature in FizzbuzzService

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -10,5 +10,10 @@ describe("Unit tests for Fizzbuzz Service", () => {
         expect(FizzbuzzService.applyValidationInExplorer(explorer5)).toStrictEqual({name: "Explorer5", score: 5, trick: "BUZZ"});
         const explorer15 = {name: "Explorer15", score: 15};
         expect(FizzbuzzService.applyValidationInExplorer(explorer15)).toStrictEqual({name: "Explorer15", score: 15, trick: "FIZZBUZZ"});    
+        
+        expect(FizzbuzzService.applyValidationInNumber(1)).toBe(1);
+        expect(FizzbuzzService.applyValidationInNumber(3)).toBe("FIZZ");
+        expect(FizzbuzzService.applyValidationInNumber(5)).toBe("BUZZ");
+        expect(FizzbuzzService.applyValidationInNumber(15)).toBe("FIZZBUZZ");
     });
 });
