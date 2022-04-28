d44f9d94da62efa96ff7332f6b417060fa4fc9ac
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-04-27 20:18:27 -0500

    update: tests to use test-data file

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -4,7 +4,7 @@ const Reader = require("../../lib/utils/Reader");
 
 describe("Unit tests for Explorer Service", () => {
     test("Use the explorer Service", () => {
-        const explorers = Reader.readJsonFile("explorers.json");
+        const explorers = Reader.readJsonFile("./test/data/test-data.json");
         expect(ExplorerService.filterByMission(explorers, "node").length).toBe(10);
         expect(ExplorerService.getAmountOfExplorersByMission(explorers, "node")).toBe(10);
         expect(ExplorerService.getExplorersUsernamesByMission(explorers, "node").length).toBe(10);
--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -2,7 +2,7 @@ const Reader = require("../../lib/utils/Reader");
 
 describe("Unit tests for Reader class", () => {
     test("Create a Reader object", () => {
-        const explorers = Reader.readJsonFile("explorers.json");
+        const explorers = Reader.readJsonFile("./test/data/test-data.json");
         expect(explorers.length).toBe(15);
     });
 });
