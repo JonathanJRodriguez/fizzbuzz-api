3b6429719f01e1528cc0edfa9475be251f9b22e3
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-04-28 18:20:09 -0500

    add: Explorer controller test for fizzbuzz

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -5,5 +5,9 @@ describe("Unit tests for Explorer Controller", () => {
         expect(ExplorerController.getExplorersByMission("node").length).toBe(10);
         expect(ExplorerController.getExplorersUsernamesByMission("node").length).toBe(10);
         expect(ExplorerController.getExplorersAmountByMission("node")).toBe(10);
+        expect(ExplorerController.getFizzbuzzByNumber(1)).toBe(1);
+        expect(ExplorerController.getFizzbuzzByNumber(3)).toBe("FIZZ");
+        expect(ExplorerController.getFizzbuzzByNumber(5)).toBe("BUZZ");
+        expect(ExplorerController.getFizzbuzzByNumber(15)).toBe("FIZZBUZZ");
     });
 });
